21d9dee2b0078ba3cf82730cac961b47d8f4e5f3
thirureddy-git <reachreddy@gmail.com>
2022-10-03 18:31:38 -0500

    Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'profile';
+  titleMain = 'Profile Editor'
+  title = 'User Information';
+
+  user:User = {name:"Jordon",age:40,favoritecolor:"blue", birthday: new Date, caffeine:"Water"};
+
+// user:User[] = [
+//   {name:"Jordon",age:40,favoritecolor:"blue"}
+//   // {name:"Kobe",age:35,favoritecolor:"green"},
+//   // {name:"Duncan",age:36,favoritecolor:"yellow"},
+//   // {name:"Tom",age:41,favoritecolor:"orange"}
+// ]
+
+displayEdit:boolean = false;
+
+toggleEdit(): void {
+this.displayEdit = !this.displayEdit;
+}
+
 }
